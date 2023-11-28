d35c4de20c8c1af63caa9412fb81882a35acd9fb
guadacastro <guadycasmar123@gmail.com>
2023-11-28 15:23:20 -0300

    update

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,11 +7,12 @@ const NavBar = () => {
         <nav className='xl-auto flex flex-row justify-evenly items-center gap-2 my-6'>
             <NavLink to='/' className='flex flex-row items-center gap-2'>
                 <img src={logo} alt="Logo" className='w-8' />
-                <p>T-Shirt.AI</p>
+                <p className='font-bold'>T-Shirt.AI</p>
             </NavLink>
             <div className='flex flex-row gap-8 font-sans text-xl'>
-            <NavLink to={`/`} activeClassName='active' className='py-1 px-4 rounded-md hover:bg-gray-50'>Home</NavLink>
+                <NavLink to={`/`} activeClassName='active' className='py-1 px-4 rounded-md hover:bg-gray-50'>Home</NavLink>
                 <NavLink to={`/category/women's clothing`} activeClassName='active' className='py-1 px-4 rounded-md hover:bg-gray-50'>Women's Clothing</NavLink>
+                <NavLink to={`/category/men's clothing`} activeClassName='active' className='py-1 px-4 rounded-md hover:bg-gray-50'>Men's Clothing</NavLink>
                 <NavLink to={`/category/electronics`} activeClassName='active' className='py-1 px-4 rounded-md hover:bg-gray-50'>Electronics</NavLink>
                 <NavLink to={`/category/jewelery`} activeClassName='active' className='py-1 px-4 rounded-md hover:bg-gray-50'>Jewelery</NavLink>
                 {/* Agrega más NavLink según tus categorías */}
---
+++ src/pages/Design.js
@@ -0,0 +1,1 @@
+//Esta va a ser la pagina para poder hacer tu propia remera o buzo con un dibujo creado con Dall-e 3
