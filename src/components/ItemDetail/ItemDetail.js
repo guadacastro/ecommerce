5e7e06348f776f97e8d04fa9103dba3aba86971f
guadacastro <guadycasmar123@gmail.com>
2023-12-18 22:22:47 -0300

    update

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -22,14 +22,14 @@ const ItemDetail = ({id, title, image, category, description, price, stock}) => 
 
     return (
         <article className='flex flex-wrap justify-center items-start md:items-center py-10'>
-            <div className="flex justify-center w-full md:w-1/2 px-4">
+            <div className="flex justify-center w-full md:w-1/2 px-20 py-20 ">
                 <img className='max-w-xs md:max-w-md lg:max-w-lg xl:max-w-xl mx-auto' src={image} alt={title} />
             </div>
             <div className="w-full md:w-1/2 px-4">
                 <section className='mb-6'>
                     <h2 className='text-2xl font-bold mb-3'>{title}</h2>
                     <p className='text-sm text-gray-500 mb-1 uppercase tracking-wide'>{category}</p>
-                    <p className='text-gray-700 mb-3'>{description}</p>
+                    <p className='text-gray-700 mb-3 mr-36'>{description}</p>
                     <p className='text-lg font-semibold'>${price}</p>
                 </section>
                 <div>
